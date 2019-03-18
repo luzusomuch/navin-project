d1fc025e5f3f8db2a2bd78312911a7b016c92202
Vinh Nguyen <luzusomuch@gmail.com>
2019-03-18 12:34:09 +0700

    using real API URI

--- src/app/service.ts
+++ src/app/service.ts
@@ -5,12 +5,15 @@ const serviceApiKey = "6d5d56f7cf494134a96cc6910549341a";
 const headers = {
   'x-authentication': serviceApiKey
 };
+const API_URI = 'https://API.com/database/v1/consumer/get-enrollment-dispute-data';
+const caseId = 'D0001400016';
 
 @Injectable()
 export class AppService {
   constructor(private http: HttpClient) { }
 
   getListTabs(params: any = {}) {
-    return this.http.get(`assets/sample.json`, {headers, params});
+    return this.http.post(API_URI, {caseId}, {headers});
+    // return this.http.get(`assets/sample.json`, {headers, params});
   }
 }
